964d5131e1b2920b2b14ba686ae7b5e6dc77dc6f
hnwywwh <34830631+hnwywwh@users.noreply.github.com>
2026-03-23 18:09:33 +0800

    Modify feed sources in diy-part1.sh
    Updated feed sources to use ImmortalWRT repositories.

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i 's/src-git packages https:\/\/github.com\/openwrt\/packages.git;openwrt-24.10/src-git packages https:\/\/github.com\/immortalwrt\/packages.git;openwrt-24.10/g' feeds.conf.default
+sed -i 's/src-git luci https:\/\/github.com\/openwrt\/luci.git;openwrt-24.10/src-git luci https:\/\/github.com\/immortalwrt\/luci.git;openwrt-24.10/g' feeds.conf.default
